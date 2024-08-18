624979f647d6b038a1ee18079cd5c9f2caa3a4ab
santiarco92 <santiago_arco@hotmail.com>
2024-08-18 12:18:47 -0500

    Project Completed

--- index.js
+++ index.js
@@ -15,6 +15,31 @@ app.get("/",  (req, res) => {
     res.render("index.ejs");
 });
 
+app.get("/submit", async (req,res) => {
+    try {
+        const response = await axios.get(" https://aphorismcookie.herokuapp.com");
+        const result = response.data;
+    
+
+        console.log( "this is the first result: " + result.data["message"]); 
+
+        res.render("index.ejs", {
+            message: result.data["message"],
+        });
+        
+    }catch (error) {
+        console.error(error);
+        res.status(500).send('Error al obtener el mensaje');
+    }
+   
+});
+
+app.get("/refresh", (req, res) => {
+    res.render("index.ejs", {
+        message: undefined,
+    });
+});
+
 
 
 app.listen(port, () => {
